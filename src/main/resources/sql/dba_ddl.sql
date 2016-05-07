0debaba51cab44cbec210be239a24abb65a7358d
Michael Saunders <msaun@relational.com.au>
2016-05-07 11:42:54 +0930

    * Setup a development password for the user.

--- src/main/resources/sql/dba_ddl.sql
+++ src/main/resources/sql/dba_ddl.sql
@@ -1,5 +1,5 @@
 ---------- database
-create role summit WITH LOGIN;
+create role summit WITH LOGIN password 'summit_dev';
 
 create tablespace SUMMIT OWNER summit LOCATION '/var/lib/postgres/tablespaces/summit';
 
