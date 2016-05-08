c80c3bfc2e3a1ed6d5ecd44d684538068e972c80
Michael Saunders <msaun@relational.com.au>
2016-05-08 17:00:26 +0930

    * The controller mapping was incorrect

--- src/main/java/org/awiki/kamikaze/summit/controller/RenderPageController.java
+++ src/main/java/org/awiki/kamikaze/summit/controller/RenderPageController.java
@@ -24,7 +24,7 @@ public class RenderPageController {
    * This renders the application pages to a String
    * TODO FIXME obviously this needs to be improved greatly, but will do for testing.
    */
-  @RequestMapping(value = "/run/${applicationId}/${pageId}", method = RequestMethod.GET)
+  @RequestMapping(value = "/run/{applicationId}/{pageId}", method = RequestMethod.GET)
   public String view(@PathVariable String applicationId, @PathVariable String pageId) {
     logger.info("Hit page /run/" + applicationId + "/" + pageId);
     return renderService.renderPageAsString(Long.parseLong(applicationId), Long.parseLong(pageId));
