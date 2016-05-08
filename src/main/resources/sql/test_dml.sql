dcef9b5a6d64fffabb382b6fa42c59ff0db4e775
Michael Saunders <msaun@relational.com.au>
2016-05-08 17:18:31 +0930

    * Fix up the test DML further. Now runs OK.
    TODO: Oh crap, changed the DTOs at some point and now they look like they don't match the domains. Will have to come back to this when I have more time...

--- src/main/resources/sql/test_dml.sql
+++ src/main/resources/sql/test_dml.sql
@@ -3,7 +3,7 @@
 --unused at this time
 --insert into applicatio_schemas values ();
 
-set autocommit off;
+set autocommit to off;
 
 insert into application select nextval('application_seq'), 1, 'Test Application';
 
@@ -14,16 +14,16 @@ insert into page (select nextval('page_seq'), id, 'Test Report Page' from templa
 insert into application_page (select nextval('spare_seq'), id, (select id from page where name = 'Test Report Page'), 1 from application where name = 'Test Application');
 
 -- TODO review this, seems odd being the same code as source_type
-insert into code_source_type values ('dml_report', 'text/plain; sql; dml; report', 1, 'text/plain; sql; dml; report');
+insert into code_source_type values ('dml_report', 'text/plain; sql; dml; report', 1, 'dml_report');
 
 -- TODO review this, seems odd being the same code as source_type
 --insert into code_processing_type values ('dml_report', 'text/plain; sql; dml; report', 1, 'dml_report');
 
-insert into code_region_type values ('Report', 'Report Region', 1, 'text/plain; sql; dml; report');
+insert into code_region_type values ('Report', 'Report Region', 1, 'dml_report');
 
 insert into code_region_position values ('body1', 'Top most region on template body', 3);
 
-insert into region  (select nexval('region_seq'), 'Test Report Region', 'body1', 'Report', 'text/plain; sql; dml; report');
+insert into region  (select nextval('region_seq'), 'Test Report Region', 'body1', 'Report', 'text/plain; sql; dml; report');
 
 insert into page_region (select nextval('spare_seq'), (select id from page where name = 'Test Report Page'), id, 1 from region where name ='Test Report Region');
 
