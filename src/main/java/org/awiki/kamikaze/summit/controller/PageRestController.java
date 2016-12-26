2394e3c741346178e4d997636daffa4ac734fe69
Michael Saunders <msaun@relational.com.au>
2016-12-26 22:20:30 +0930

    Pass through the needed params for pagination

--- src/main/java/org/awiki/kamikaze/summit/controller/PageRestController.java
+++ src/main/java/org/awiki/kamikaze/summit/controller/PageRestController.java
@@ -47,7 +47,10 @@ public class PageRestController {
      */
     
     logger.info("Hit page /api/filter/" + regionId);
-    final PageItem<String> filteredResults = filterService.filterRegion(Long.parseLong(regionId), searchValue);
+    final PageItem<String> filteredResults = filterService.filterRegion(Long.parseLong(regionId), searchValue, 
+            (pageNumber != null ? Long.parseLong(pageNumber) : 0), 
+            (rowsPerPage != null ? Long.parseLong(rowsPerPage) : 0) 
+           );
     return (SourceProcessorResultTable)filteredResults;
   }
 	
